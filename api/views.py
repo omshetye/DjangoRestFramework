e66b85392d55518de030d56a2479521b1526319a
omshetye <omega10303@gmail.com>
2025-08-09 14:18:38 +0530

    created serializer for accessing  student model via get request

--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
+from students.models import Student
+from . serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+# def studentsView(request):
+#   students = Student.objects.all()
+#   student_list = []
+#   for student in students.values():
+#     student_list.append(student)
+#   print(student_list)
+#   return JsonResponse(student_list, safe=False)
 
+# DRF Serialization
+
+@api_view(['GET'])
 def studentsView(request):
-  students = {
-    'id': 1,
-    'name': 'Om',
-    'age': 22
-  }
-  return JsonResponse(students)
+  if request.method == 'GET':
+    # Get all data from Student table
+    students = Student.objects.all()
+    serializer = StudentSerializer(instance=students, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
